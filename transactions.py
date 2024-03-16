d3f9b44c2027c5fe164676425d34d83639d61aaf
Bearythebee <barrylim1996@gmail.com>
2024-03-17 02:56:56 +0800

    Added Prefect

--- transactions.py
+++ transactions.py
@@ -10,8 +10,11 @@ from oauth2client.service_account import ServiceAccountCredentials
 from misc.CustomLogging import Logger, log_fuction
 from prefect import flow, task
 from prefect.server.schemas.schedules import CronSchedule
+from prefect.blocks.system import Secret
+from prefect.blocks.system import JSON
 
-__conn_str = os.environ["COCKROACH_DB"]
+
+__conn_str = Secret.load("cockroachdb").get()
 __conn  = create_engine(__conn_str).connect()
 
 
@@ -38,9 +41,7 @@ def fetch_transactions_source():
     scope =  ['https://www.googleapis.com/auth/spreadsheets',
             "https://www.googleapis.com/auth/drive"]
 
-    credentials = ServiceAccountCredentials.from_json_keyfile_name(
-                "./cred/portfolio-367803-a4d1e0b1c6a4.json",
-                scope)
+    credentials = ServiceAccountCredentials.from_json_keyfile_dict(keyfile_dict=JSON.load("googlejson").dict()['value'],scopes=scope)
 
     client = gspread.authorize(credentials)
 
@@ -88,7 +89,6 @@ def insert_delta(__transactions_source, __transactions_target):
 
         if to_ingest.is_empty():
             print('No new rows to ingest')
-            logger.info('No new rows to ingest')
         else:
             to_ingest.write_database(
                 table_name="transactions",
@@ -100,10 +100,10 @@ def insert_delta(__transactions_source, __transactions_target):
             print(f'Inserting {to_ingest.shape[0]} rows')
 
 
-@flow(retries=3, retry_delay_seconds=5, log_prints=True)
+@flow(retries=1, retry_delay_seconds=5, log_prints=True)
 def transaction_service(name: str = "transaction_service"):
-    print(f"Testing Connection to DB")
-    test_connection_db()
+    # print(f"Testing Connection to DB")
+    # test_connection_db()
     __transactions_target = fetch_transactions_target()
     __transactions_source = fetch_transactions_source()
     insert_delta(__transactions_source,__transactions_target)
@@ -114,9 +114,8 @@ if __name__ == "__main__":
         entrypoint="transactions.py:transaction_service"
     ).deploy(
         name="transaction_service",
-        work_pool_name="PortfolioMonitoring"
-        
-        )
-        # tags=["transaction"],
-        # parameters={},
-        # schedule=(CronSchedule(cron="0 0 * * *", timezone="Asia/Singapore")),
+        work_pool_name="PortfolioMonitoring",
+        tags=["transaction"],
+        parameters={},
+        schedule=(CronSchedule(cron="0 0 * * *", timezone="Asia/Singapore"))
+    )
